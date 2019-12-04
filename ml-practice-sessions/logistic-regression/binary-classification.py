b7e8a525786314ce8205fbdebdef020e834c441e
Jaya Shankar Vadisela <jvadisela@gmail.com>
2019-12-04 16:29:26 -0500

    added ml-practice-sessions project

---
+++ ml-practice-sessions/logistic-regression/binary-classification.py
@@ -0,0 +1,135 @@
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName('ml-bank').getOrCreate()
+df = spark.read.csv('bank.csv', header = True, inferSchema = True)
+df.printSchema()
+
+import pandas as pd
+pd = pd.DataFrame(df.take(5), columns=df.columns)
+print pd
+
+transposed = pd.transpose()
+print transposed
+
+numeric_features = [t[0] for t in df.dtypes if t[1] == 'int']
+stats = df.select(numeric_features).describe().toPandas().transpose()
+print stats
+
+numeric_data = df.select(numeric_features).toPandas()
+#axs = pd.scatter_matrix(numeric_data, figsize=(8, 8))
+
+# n = len(numeric_data.columns)
+# for i in range(n):
+#     v = axs[i, 0]
+#     v.yaxis.label.set_rotation(0)
+#     v.yaxis.label.set_ha('right')
+#     v.set_yticks(())
+#     h = axs[n-1, i]
+#     h.xaxis.label.set_rotation(90)
+#     h.set_xticks(())
+
+#from pandas.plotting import scatter_matrix
+#scatter_matrix(numeric_data, alpha=0.2, figsize=(8, 8))
+#plt.show()
+
+df = df.select('age', 'job', 'marital', 'education', 'default', 'balance', 'housing', 'loan', 'contact', 'duration', 'campaign', 'pdays', 'previous', 'poutcome', 'deposit')
+cols = df.columns
+df.printSchema()
+
+from pyspark.ml.feature import OneHotEncoderEstimator, StringIndexer, VectorAssembler
+categoricalColumns = ['job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'poutcome']
+stages = []
+for categoricalCol in categoricalColumns:
+    stringIndexer = StringIndexer(inputCol = categoricalCol, outputCol = categoricalCol + 'Index')
+    encoder = OneHotEncoderEstimator(inputCols=[stringIndexer.getOutputCol()], outputCols=[categoricalCol + "classVec"])
+    stages += [stringIndexer, encoder]
+label_stringIdx = StringIndexer(inputCol = 'deposit', outputCol = 'label')
+stages += [label_stringIdx]
+numericCols = ['age', 'balance', 'duration', 'campaign', 'pdays', 'previous']
+assemblerInputs = [c + "classVec" for c in categoricalColumns] + numericCols
+assembler = VectorAssembler(inputCols=assemblerInputs, outputCol="features")
+stages += [assembler]
+
+from pyspark.ml import Pipeline
+pipeline = Pipeline(stages = stages)
+pipelineModel = pipeline.fit(df)
+df = pipelineModel.transform(df)
+selectedCols = ['label', 'features'] + cols
+df = df.select(selectedCols)
+df.printSchema()
+
+df.show()
+
+train, test = df.randomSplit([0.7, 0.3], seed = 2018)
+print("Training Dataset Count: " + str(train.count()))
+print("Test Dataset Count: " + str(test.count()))
+
+from pyspark.ml.classification import LogisticRegression
+lr = LogisticRegression(featuresCol = 'features', labelCol = 'label', maxIter=10)
+lrModel = lr.fit(train)
+
+import matplotlib.pyplot as plt
+import numpy as np
+beta = np.sort(lrModel.coefficients)
+plt.plot(beta)
+plt.ylabel('Beta Coefficients')
+plt.show()
+
+
+trainingSummary = lrModel.summary
+roc = trainingSummary.roc.toPandas()
+plt.plot(roc['FPR'],roc['TPR'])
+plt.ylabel('False Positive Rate')
+plt.xlabel('True Positive Rate')
+plt.title('ROC Curve')
+plt.show()
+print('Training set areaUnderROC: ' + str(trainingSummary.areaUnderROC))
+
+
+pr = trainingSummary.pr.toPandas()
+plt.plot(pr['recall'],pr['precision'])
+plt.ylabel('Precision')
+plt.xlabel('Recall')
+plt.show()
+
+
+predictions = lrModel.transform(test)
+predictions.select('age', 'job', 'label', 'rawPrediction', 'prediction', 'probability').show(10)
+
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+evaluator = BinaryClassificationEvaluator()
+print('Test Area Under ROC', evaluator.evaluate(predictions))
+print("Test Area Under ROC: " + str(evaluator.evaluate(predictions, {evaluator.metricName: "areaUnderROC"})))
+
+from pyspark.ml.classification import RandomForestClassifier
+rf = RandomForestClassifier(featuresCol = 'features', labelCol = 'label')
+rfModel = rf.fit(train)
+predictions = rfModel.transform(test)
+predictions.select('age', 'job', 'label', 'rawPrediction', 'prediction', 'probability').show(10)
+print("Test Area Under ROC: " + str(evaluator.evaluate(predictions, {evaluator.metricName: "areaUnderROC"})))
+
+
+from pyspark.ml.classification import GBTClassifier
+gbt = GBTClassifier(maxIter=10)
+gbtModel = gbt.fit(train)
+predictions = gbtModel.transform(test)
+predictions.select('age', 'job', 'label', 'rawPrediction', 'prediction', 'probability').show(10)
+print("Test Area Under ROC: " + str(evaluator.evaluate(predictions, {evaluator.metricName: "areaUnderROC"})))
+
+print(gbt.explainParams())
+
+
+from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
+paramGrid = (ParamGridBuilder()
+             .addGrid(gbt.maxDepth, [2, 4, 6])
+             .addGrid(gbt.maxBins, [20, 60])
+             .addGrid(gbt.maxIter, [10, 20])
+             .build())
+cv = CrossValidator(estimator=gbt, estimatorParamMaps=paramGrid, evaluator=evaluator, numFolds=5)
+# Run cross validations.  This can take about 6 minutes since it is training over 20 trees!
+cvModel = cv.fit(train)
+predictions = cvModel.transform(test)
+evaluator.evaluate(predictions)
